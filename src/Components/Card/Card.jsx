c91265a8e481fb31a956c43bb6bf1afee32ddd9e
maharit108 <maha.rit108@gmail.com>
2021-01-10 21:38:18 -0600

    Add images and email to monsters.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -5,7 +5,9 @@ import './Card.css'
 function Card(props) {
     return (
         <div className='card__container'>
-            <h1>{props.monster.name}</h1>
+            <img alt={`monster${props.monster.name}`} src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+            <h2>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
         </div>
     )
 }
